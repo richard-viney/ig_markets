74c1ef5053f44d251abc2583956d071d6e188332
Richard Viney <richard.viney@gmail.com>
2015-07-22 13:43:40 +1200

    Improved PasswordEncryptor spec

--- spec/ig_markets/password_encryptor_spec.rb
+++ spec/ig_markets/password_encryptor_spec.rb
@@ -1,15 +1,16 @@
 describe IGMarkets::PasswordEncryptor do
+  let(:encryptor) { IGMarkets::PasswordEncryptor.new }
   let(:rsa_key_pair) { OpenSSL::PKey::RSA.new 2048 }
-  let(:time_stamp) { '1000' }
-  let(:encryptor) do
-    IGMarkets::PasswordEncryptor.new.tap do |e|
-      e.encoded_public_key = Base64.strict_encode64 rsa_key_pair.to_pem
-      e.time_stamp = time_stamp
-    end
+
+  it 'can set its public key from an encoded public key' do
+    expect { encryptor.encoded_public_key = Base64.strict_encode64 rsa_key_pair.to_pem }.not_to raise_error
   end
 
-  it 'encrypts a password' do
+  it 'can encrypt a password' do
     password = 'test'
+
+    encryptor.time_stamp = '1000'
+    encryptor.public_key = rsa_key_pair
 
     encoded_encrypted_password = encryptor.encrypt password
 
@@ -17,6 +18,6 @@ describe IGMarkets::PasswordEncryptor do
     encoded_decrypted_password = rsa_key_pair.private_decrypt decoded_encrypted_password
     decoded_decrypted_password = Base64.strict_decode64 encoded_decrypted_password
 
-    expect(decoded_decrypted_password).to eq("#{password}|#{time_stamp}")
+    expect(decoded_decrypted_password).to eq("#{password}|#{encryptor.time_stamp}")
   end
 end
