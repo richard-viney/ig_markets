ba54b44242a0f4ba0e1408b3b2c05008da803031
Richard Viney <richard.viney@gmail.com>
2015-08-20 00:45:24 +1200

    Added missing attributes to IGMarkets::Application

--- lib/ig_markets/application.rb
+++ lib/ig_markets/application.rb
@@ -1,13 +1,21 @@
 module IGMarkets
   class Application < Model
+    attribute :id
+    attribute :client_id
+    attribute :name
+    attribute :tier
+    attribute :api_key
+    attribute :status
+    attribute :allowance_application_overall, type: :float
+    attribute :allowance_account_trading, type: :float
+    attribute :allowance_account_overall, type: :float
+    attribute :allowance_account_historical_data, type: :float
+    attribute :concurrent_subscriptions_limit
+    attribute :created_date
     attribute :allow_equities, type: :boolean
     attribute :allow_quote_orders, type: :boolean
-    attribute :allowance_account_historical_data, type: :float
-    attribute :allowance_account_overall, type: :float
-    attribute :allowance_account_trading, type: :float
-    attribute :allowance_application_overall, type: :float
-    attribute :api_key
-    attribute :name
-    attribute :status
+    attribute :restricted_to_self
+    attribute :terms_accepted_date
+    attribute :fast_markets_settlement_price_enabled, type: :boolean
   end
 end
