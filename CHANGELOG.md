0ef5f458d26c20a8527fb02ca92634d7fd0f86c4
Richard Viney <richard.viney@gmail.com>
2016-04-09 02:04:53 +1200

    Bump master to v0.2.pre and add CHANGELOG.md

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# IG Markets Changelog
+
+### 0.2 - Unreleased
+
+- ...
+
+### 0.1 - April 8, 2016
+
+- Initial release
--- lib/ig_markets/version.rb
+++ lib/ig_markets/version.rb
@@ -1,4 +1,4 @@
 module IGMarkets
   # The version of this gem.
-  VERSION = '0.1'.freeze
+  VERSION = '0.2.pre'.freeze
 end
