f49758150e7547dd4c3156ef61561950aaf7b142
Richard Viney <richard.viney@gmail.com>
2016-04-09 01:19:25 +1200

    Added #interest?, #proft_and_loss_amount, and #formatted_transaction_type to AccountTransaction

--- lib/ig_markets/account_transaction.rb
+++ lib/ig_markets/account_transaction.rb
@@ -14,5 +14,36 @@ module IGMarkets
     attribute :reference
     attribute :size, String, nil_if: '-'
     attribute :transaction_type, Symbol, allowed_values: [:deal, :depo, :dividend, :exchange, :with]
+
+    # Returns whether or not this transaction was an interest payment. Interest payments can be either deposits or
+    # withdrawals depending on the underlying instrument and currencies involved. Interest payments are identified by
+    # the presence of the word `interest` in {#instrument_name}.
+    #
+    # @return [Boolean]
+    def interest?
+      [:depo, :with].include?(transaction_type) && !(instrument_name.downcase =~ /(^|[^a-z])interest([^a-z]|$)/).nil?
+    end
+
+    # Returns this transaction's {#profit_and_loss} as a `Float`, denominated in this transaction's {#currency}.
+    #
+    # @return [Float]
+    def profit_and_loss_amount
+      raise 'profit_and_loss does not start with the expected currency' unless profit_and_loss.start_with? currency
+
+      profit_and_loss[currency.length..-1].delete(',').to_f
+    end
+
+    # Returns this transaction's {#transaction_type} as a human-readable string.
+    #
+    # @return [String]
+    def formatted_transaction_type
+      {
+        deal: 'Deal',
+        depo: 'Deposit',
+        dividend: 'Dividend',
+        exchange: 'Exchange',
+        with: 'Withdrawal'
+      }.fetch transaction_type
+    end
   end
 end
---
+++ spec/ig_markets/account_transaction_spec.rb
@@ -0,0 +1,30 @@
+describe IGMarkets::DealingPlatform do
+  it 'correctly identifies interest payments' do
+    {
+      { transaction_type: :with, instrument_name: 'interest' } => true,
+      { transaction_type: :depo, instrument_name: ',interest' } => true,
+      { transaction_type: :with, instrument_name: 'interest,' } => true,
+      { transaction_type: :depo, instrument_name: ',interest,' } => true,
+      { transaction_type: :with, instrument_name: 'pinterest' } => false,
+      { transaction_type: :depo, instrument_name: 'interests' } => false
+    }.each do |attributes, result|
+      expect(build(:account_transaction, attributes).interest?).to eq(result)
+    end
+  end
+
+  it 'reports correct profit/loss amounts' do
+    expect(build(:account_transaction, currency: 'USD', profit_and_loss: 'USD10.10').profit_and_loss_amount).to eq(10.1)
+    expect(build(:account_transaction, currency: 'USD', profit_and_loss: 'USD-5.50').profit_and_loss_amount).to eq(-5.5)
+
+    expect { build(:account_transaction, currency: 'USD', profit_and_loss: 'EUR-5.0').profit_and_loss_amount }
+      .to raise_error(StandardError)
+  end
+
+  it 'formats transaction types' do
+    expect(build(:account_transaction, transaction_type: :deal).formatted_transaction_type).to eq('Deal')
+    expect(build(:account_transaction, transaction_type: :depo).formatted_transaction_type).to eq('Deposit')
+    expect(build(:account_transaction, transaction_type: :dividend).formatted_transaction_type).to eq('Dividend')
+    expect(build(:account_transaction, transaction_type: :exchange).formatted_transaction_type).to eq('Exchange')
+    expect(build(:account_transaction, transaction_type: :with).formatted_transaction_type).to eq('Withdrawal')
+  end
+end
