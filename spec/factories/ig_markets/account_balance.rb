3c6d837c601307eaec50906b2ce6a82277bd3091
Richard Viney <richard.viney@gmail.com>
2016-04-12 03:22:23 +1200

    Minor spec update

--- spec/factories/ig_markets/account_balance.rb
+++ spec/factories/ig_markets/account_balance.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :account_balance, class: IGMarkets::Account::Balance do
-    available 5000.0
-    balance 5000.0
+    available 500.0
+    balance 500.0
     deposit 0.0
     profit_loss 0.0
   end
