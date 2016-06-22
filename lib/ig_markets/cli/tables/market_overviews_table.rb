7cdc0e2353fd28f9208915849899132e908539f4
Richard Viney <richard.viney@gmail.com>
2016-06-23 03:38:56 +1200

    Swap the first two columns in MarketOverviewsTable

--- lib/ig_markets/cli/tables/market_overviews_table.rb
+++ lib/ig_markets/cli/tables/market_overviews_table.rb
@@ -9,7 +9,7 @@ module IGMarkets
       end
 
       def headings
-        ['Type', 'EPIC', 'Instrument', 'Status', 'Expiry', 'Bid', 'Offer', 'High', 'Low', 'Change (net)', 'Change (%)']
+        ['EPIC', 'Type', 'Instrument', 'Status', 'Expiry', 'Bid', 'Offer', 'High', 'Low', 'Change (net)', 'Change (%)']
       end
 
       def right_aligned_columns
@@ -17,8 +17,8 @@ module IGMarkets
       end
 
       def row(market_overview)
-        [market_overview.instrument_type, market_overview.epic, market_overview.instrument_name,
-         market_status(market_overview), market_overview.expiry, levels(market_overview)]
+        [market_overview.epic, market_overview.instrument_type, market_overview.instrument_name,
+         market_overview.market_status, market_overview.expiry, levels(market_overview)]
       end
 
       def cell_color(value, _model, _row_index, column_index)
@@ -35,12 +35,6 @@ module IGMarkets
         [:bid, :offer, :high, :low, :net_change, :percentage_change].map do |attribute|
           Format.level market_overview.send(attribute)
         end
-      end
-
-      def market_status(market_overview)
-        { closed: 'Closed', edits_only: 'Edits only', offline: 'Offline', on_auction: 'On auction',
-          on_auction_no_edits: 'On auction no edits', suspended: 'Suspended', tradeable: 'Tradeable' }
-          .fetch market_overview.market_status
       end
     end
   end
